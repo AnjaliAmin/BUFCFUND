7266a75961b2a15b83264144c2f8b03f1f1dee6d
AnjaliAmin <113141108+AnjaliAmin@users.noreply.github.com>
2025-10-06 16:03:35 -0400

    Update application.py

--- application.py
+++ application.py
@@ -146,4 +146,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True, use_reloader=False, dev_tools_hot_reload=False)
+    app.run(host='0.0.0.0', port=8000, debug=False, use_reloader=False, dev_tools_hot_reload=False)
